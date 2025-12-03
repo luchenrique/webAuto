6f987f717b17517ee80a37bea9d019eb3038d181
Lucas <lucas.barbosa3680@gmail.com>
2025-12-02 23:30:38 -0300

    first input

--- auto.py
+++ auto.py
@@ -7,3 +7,8 @@ servico = Service(ChromeDriverManager().install())
 
 navegador = webdriver.Chrome(service=servico)
 
+navegador.get("https://docs.google.com/forms/d/e/1FAIpQLSe8FPkMAr5tG4OzWtidLGZNX7NrmRg-uQ1QR8eq37S9yZ7YTQ/viewform?usp=dialog")
+navegador.find_element('xpath', '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[1]/div/div/div[2]/div/div[1]/div/div[1]/input').send_keys("Lucas")
+
+
+input("Pressione ENTER para sair...")
